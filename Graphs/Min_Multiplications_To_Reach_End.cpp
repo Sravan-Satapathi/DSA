c0ac924a49db77c6a37dc393972dafac0f28ab70
Sravan <ssaisravan2004@gmail.com>
2025-07-04 22:13:42 +0530

    Minimum Multiplications to reach end

--- Graphs/Min_Multiplications_To_Reach_End.cpp
+++ Graphs/Min_Multiplications_To_Reach_End.cpp
@@ -1,10 +1,7 @@
-// User function Template for C++
-
 class Solution {
   public:
     int minimumMultiplications(vector<int>& arr, int start, int end) {
         // code here
-        if(start==end) return 0;
         vector<int> dist(1e5,INT_MAX);
         queue<pair<int,int>> q;
         q.push({0,start});
@@ -13,12 +10,12 @@ class Solution {
             int node=q.front().second;
             int steps=q.front().first;
             q.pop();
+            if(node==end) return steps;
             for(int i=0;i<arr.size();i++){
                 long long el=arr[i];
                 int neighbour=(node*el)%mod;
                 if(steps+1<dist[neighbour]){
                     dist[neighbour]=steps+1;
-                    if(neighbour==end) return steps+1;
                     q.push({dist[neighbour],neighbour});
                 }
             }
