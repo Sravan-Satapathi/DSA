9be587186b643c6b181d908ed2e92ed96f8c0db5
Sravan <ssaisravan2004@gmail.com>
2025-07-22 17:10:25 +0530

    Update Detect_Cycle_LL.cpp

--- LinkedList/Detect_Cycle_LL.cpp
+++ LinkedList/Detect_Cycle_LL.cpp
@@ -6,6 +6,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//Detect Cycle
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -17,5 +19,29 @@ public:
             if(slow==fast) return true;
         }
         return false;
+    }
+};
+
+
+//Find length of cycle if exists
+class Solution {
+public:
+    int lengthOfCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL) return false;
+        ListNode *slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                int count=1;
+                fast=fast->next;
+                while(fast!=slow){
+                    fast=fast->next;
+                    count++;
+                }
+                return count;
+            }
+        }
+        return 0;
     }
 };
