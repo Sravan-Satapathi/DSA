e83fdf996f7c1d6bbcd970ab5c5c4fea484e0a66
Sravan <ssaisravan2004@gmail.com>
2025-07-22 16:52:32 +0530

    Update Reverse_LL.cpp

--- LinkedList/Reverse_LL.cpp
+++ LinkedList/Reverse_LL.cpp
@@ -27,3 +27,14 @@ public:
 
 
 //Recursive
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr || head->next==nullptr) return head;
+        ListNode* newHead=reverseList(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=nullptr;
+        return newHead;
+    }
+};
