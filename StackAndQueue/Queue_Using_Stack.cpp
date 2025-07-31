441890dfad473b41a764b6fbd6e2df3a16794458
Sravan <ssaisravan2004@gmail.com>
2025-07-31 10:26:14 +0530

    Update Queue_Using_Stack.cpp

--- StackAndQueue/Queue_Using_Stack.cpp
+++ StackAndQueue/Queue_Using_Stack.cpp
@@ -1,10 +1,10 @@
+//Approach 1
 class MyQueue {
 private:
     stack<int> st1,st2;
 public:
     MyQueue() {
     }
-
     //TC - O(2N)
     void push(int x) {
         while(!st1.empty()){
@@ -34,3 +34,48 @@ public:
 };
 
 
+//Approach 2
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyQueue {
+private:
+    stack<int> st1,st2;
+public:
+    MyQueue() {
+    }
+    //TC - O(1)
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        if(!st2.empty()){
+            int el=st2.top();
+            st2.pop();
+            return el;
+        }
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int el=st2.top();
+        st2.pop();
+        return el;
+    }
+    
+    int peek() {
+        if(!st2.empty()){
+            return st2.top();
+        }
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        return st2.top();
+    }
+    
+    bool empty() {
+        return st1.empty() && st2.empty();
+    }
+};
